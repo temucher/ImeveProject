925460210f8052b979a6942aa04640620b7c1107
Teagan Mucher <tjsmuch@gmail.com>
2018-03-22 16:15:15 -0700

    Got the initial base html up and running

--- mysite/clips/models.py
+++ mysite/clips/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Clip(models.Model):
     scene = models.CharField(max_length=10)
     take = models.CharField(max_length=10)
-    starRating = models.CharField(max_length=10)
+    starRating = models.CharField(max_length=10) # potentially make this an IntegerField and map that to star count
 
 def __str__(self):
     return self.scene
--- mysite/clips/views.py
+++ mysite/clips/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Clip
 # Create your views here.
+
+def base(request):
+    return render(request, 'clips/base.html', context=None)
