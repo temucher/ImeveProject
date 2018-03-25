9511fba41421e2c44a03a4b2f39c092a392b962f
Teagan Mucher <tjsmuch@gmail.com>
2018-03-24 21:48:12 -0700

    Making progress on layout

--- mysite/clips/urls.py
+++ mysite/clips/urls.py
@@ -5,8 +5,3 @@ urlpatterns = [
     path('', views.video_list, name='video_list'),
     path('clips/<int:pk>', views.video_detail, name='video_detail')
 ]
-
-'''
-    path('clips/<int:pk>', views.video_reg, name='video_reg'),
-    path('clips/<int:pk>', views.video_360, name='video_360')
-    '''
--- mysite/clips/views.py
+++ mysite/clips/views.py
@@ -2,27 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Clip
 # Create your views here.
 
-''' Not sure if needed
-def base(request):
-    clips = Clip.objects.order_by('scene')
-    return render(request, 'clips/base.html', {'clips': clips})
-'''
-
 
 def video_list(request):
     clips = Clip.objects.order_by('scene')
     return render(request, 'clips/video_list.html', {'clips': clips})
-
-'''
-def video_360(request, pk):
-    clip = get_object_or_404(Clip, pk=pk)
-    return render(request, 'clips/video_360.html', {'clip': clip})
-
-
-def video_reg(request, pk):
-    clip = get_object_or_404(Clip, pk=pk)
-    return render(request, 'clips/video_reg.html', {'clip': clip})
-'''
 
 def video_detail(request, pk):
     clip = get_object_or_404(Clip, pk=pk)
