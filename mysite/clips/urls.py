5a7fc5eb81c66caaf42ac0b5bd1318513db01455
Teagan Mucher <tjsmuch@gmail.com>
2018-03-25 12:34:08 -0700

    Delete button works, now need to map to if video has been watched

--- mysite/clips/urls.py
+++ mysite/clips/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.video_list, name='video_list'),
-    path('clips/<int:pk>', views.video_detail, name='video_detail')
+    path('clips/<int:pk>', views.video_detail, name='video_detail'),
+    path('clips/<int:pk>/delete', views.video_delete, name='video_delete')
 ]
--- mysite/clips/views.py
+++ mysite/clips/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Clip
 # Create your views here.
 
@@ -7,9 +7,17 @@ def video_list(request):
     clips = Clip.objects.order_by('scene')
     return render(request, 'clips/video_list.html', {'clips': clips})
 
+
 def video_detail(request, pk):
     clip = get_object_or_404(Clip, pk=pk)
     if (clip.is360video):
         return render(request, 'clips/video_360.html', {'clip': clip})
     else:
         return render(request, 'clips/video_reg.html', {'clip': clip})
+
+
+def video_delete(request, pk):
+    clip = get_object_or_404(Clip, pk=pk)
+    clip.delete()
+    return redirect('video_list')
+
