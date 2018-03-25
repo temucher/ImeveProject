2409c0c383db46c244e08d182577047b2f6c2c83
Teagan Mucher <tjsmuch@gmail.com>
2018-03-25 12:08:34 -0700

    Holy shit it kinda works
    got the 360 video to actually display a window

--- mysite/clips/models.py
+++ mysite/clips/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Clip(models.Model):
     file_path = models.CharField(max_length=50, default='clips/videos/darude.mp4')
+    thumb_path = models.CharField(max_length=50, default='clips/images/darude.png')
     scene = models.CharField(max_length=10)
     take = models.CharField(max_length=10)
     starRating = models.CharField(max_length=10) # potentially make this an IntegerField and map that to star count
