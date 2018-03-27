e619e955a9d3f3315b37d32abdebf74509ec54c1
Teagan Mucher <tjsmuch@gmail.com>
2018-03-27 15:34:44 -0700

    UI changes, still need editable star rating and nadir metadata

--- mysite/clips/models.py
+++ mysite/clips/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Clip(models.Model):
-    file_path = models.CharField(max_length=50, default='clips/videos/darude.mp4')
-    thumb_path = models.CharField(max_length=50, default='clips/images/darude.png')
+    file_path = models.FilePathField(path="clips/static/clips/videos", allow_files=True)
+    thumb_path = models.FilePathField(path="clips/static/clips/images", allow_files=True)
     scene = models.CharField(max_length=10)
     take = models.CharField(max_length=10)
-    starRating = models.CharField(max_length=10) # potentially make this an IntegerField and map that to star count
+    starRating = models.IntegerField() # potentially make this an IntegerField and map that to star count
     is360video = models.BooleanField(default=False)
 
 def __str__(self):
