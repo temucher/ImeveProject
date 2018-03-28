101448999520f9c4341e0b27a1176a1124ed1907
Teagan Mucher <tjsmuch@gmail.com>
2018-03-27 20:01:16 -0700

    test 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -115,11 +115,21 @@ USE_L10N = True
 USE_TZ = True
 
 
+PROJECT_ROOT =  os.path.dirname(os.path.abspath(__file__))
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 import django_heroku
 django_heroku.settings(locals())
