4e9a68d927d70ad468ee74fe64640fdf5a7904e7
Teagan Mucher <tjsmuch@gmail.com>
2018-03-29 15:23:30 -0700

    Modified models.py to take a CharField

--- clips/models.py
+++ clips/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Clip(models.Model):
-    file_path = models.FilePathField(path="clips/static/clips/videos", allow_files=True)
-    thumb_path = models.FilePathField(path="clips/static/clips/images", allow_files=True)
+    file_path = models.CharField(max_length=50, default='clips/videos/mayo.mp4')
+    thumb_path = models.CharField(max_length=50, default='clips/images/mayo.png')
     scene = models.CharField(max_length=10)
     take = models.CharField(max_length=10)
     starRating = models.IntegerField() # potentially make this an IntegerField and map that to star count
